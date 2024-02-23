e2988c311b5b4aac88605ddd6c5ac3a3065b61b5
LauraZoraja <lzoraja@pmfst.hr>
2024-02-23 19:00:43 +0100

    novi commit

---
+++ Vjezbe_1/koordinate.py
@@ -0,0 +1,17 @@
+x1 = input('Unesite x koordinatu: ')
+x1 = float(x1)
+y1 = input('Unesite y koordinatu: ')
+y1 = float(y1)
+x2 = input('Unesite x koordinatu: ')
+x2 = float(x2)
+y2 = input('Unesite y koordinatu: ')
+y2 = float(y2)
+k = (y2 - y1)/(x2 - x1)
+
+l = - k*x1 +y1
+p = 'p'
+if l>0:
+    p = '+'
+else:
+    p = '-'
+print('y = {}x {} {}'.format(k,p,l))
