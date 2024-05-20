d5e41eafcb07caf10e22381d92c9d51371ca9150
LauraZoraja <lzoraja@pmfst.hr>
2024-05-20 15:59:27 +0200

    fixat zadnju sliku

--- Seminar/sem.py
+++ Seminar/sem.py
@@ -14,8 +14,9 @@ class Particle:
         self.v = np.array((0.1,0.1,0.1))
         self.epolje = epolje
         self.mpolje = mpolje
-        self.E = np.array((Ex, Ey, Ez))
+        self.Ez = Ez if callable(Ez) else lambda t: Ez
         self.B = np.array((Bx,By,Bz(self.t)))
+        self.E = np.array((Ex,Ey,self.Ez(self.t)))
         self.F = self.q * (self.E + np.cross(self.v, self.B))
         self.a = self.F / self.m
         if self.q == -1:
@@ -38,7 +39,7 @@ class Particle:
             pass
     
         if self.epolje == 'promjenjivo':
-            self.E += np.array((0.,0.,self.dt))
+            self.E += np.array((0.,0.,self.dt/1000.))
         else:
             pass
 
@@ -60,8 +61,8 @@ class Particle:
             self.listaa.append(self.a)
             self.listat.append(self.t)
 
-    def plot(self):
-        ax.plot(self.listax, self.listay, self.listaz, label = '{},{} magnetsko polje i {} elektricno polje'.format(self.cestica,self.mpolje,self.epolje))
+    def plot(self,color = 'blue'):
+        ax.plot(self.listax, self.listay, self.listaz, label = '{},{} magnetsko polje i {} elektricno polje'.format(self.cestica,self.mpolje,self.epolje), color = color)
 
     def plot_reset(self):
         self.x = self.listax[0]
@@ -73,22 +74,69 @@ class Particle:
         self.listaz = [self.z]
         self.listav = [self.v]
 
-fig = plt.figure()
-ax = plt.axes(projection ='3d')
-ax.set_box_aspect([1,1,1])
+
 def Bz1(t):
     return (1./10.)*t
 
 def Bz2(t):
     return 1.
-h1  = Particle(30.,0.,0.,0.,0.,0.,Bz1,'promjenjivo', 'konstantno')
-h2 = Particle(30.,0.,0.,0.,0.,0.,Bz2,'konstantno', 'konstantno')
-h3 = Particle(30.,0.,0.,0.,0.,0.,Bz1,'promjenjivo', 'konstantno', 1)
+
+def Ez1(t):
+    return (1./1000.)*t
+
+def Ez2(t):
+    return 0.
+
+
+#konstantno magnetsko, elektron
+fig = plt.figure()
+ax = plt.axes(projection ='3d')
+ax.set_box_aspect([1,1,1])
+h1 = Particle(30.,0.,0.,Ez2,0.,0.,Bz2,'konstantno', 'konstantno')
+h2  = Particle(30.,0.,0.,Ez1,0.,0.,Bz2,'konstantno', 'promjenjivo')
 h1.euler()
 h2.euler()
-h3.euler()
-h1.plot()
-h2.plot() 
-h3.plot() 
+h1.plot('blueviolet')
+h2.plot('lightseagreen')  
+ax.legend()
+plt.show()
+
+#samo za reference
+fig = plt.figure()
+ax = plt.axes(projection ='3d')
+ax.set_box_aspect([1,1,1])
+h2  = Particle(30.,0.,0.,Ez1,0.,0.,Bz2,'konstantno', 'promjenjivo')
+h2.euler()
+h2.plot('lightseagreen')
+ax.legend()
+plt.show()
+
+
+#konstantno elektricno, usporedba + i - u promjenjivom magnetskom
+fig = plt.figure()
+ax = plt.axes(projection ='3d')
+ax.set_box_aspect([1,1,1])
+h4 = Particle(30.,0.,0.,Ez2,0.,0.,Bz2,'konstantno', 'konstantno')
+h5  = Particle(30.,0.,0.,Ez2,0.,0.,Bz1,'promjenjivo', 'konstantno')
+h6 = Particle(30.,0.,0.,Ez2,0.,0.,Bz1,'promjenjivo', 'konstantno', 1)
+h4.euler()
+h5.euler()
+h6.euler()
+h4.plot('blueviolet')
+h5.plot('deeppink') 
+h6.plot('lightseagreen') 
+ax.legend()
+plt.show()
+
+#oba promjenjiva
+fig = plt.figure()
+ax = plt.axes(projection ='3d')
+#ax.set_box_aspect([1,1,1])
+h7 = Particle(30.,0.,0.,Ez1,0.,0.,Bz1,'promjenjivo', 'promjenjivo')
+h8  = Particle(30.,0.,0.,Ez1,0.,0.,Bz1,'promjenjivo', 'promjenjivo',1)
+h7.euler()
+h8.euler()
+h7.plot('deeppink')
+h8.plot('blueviolet') 
 ax.legend()
 plt.show()
